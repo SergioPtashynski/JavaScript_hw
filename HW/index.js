7c4a642dc5aa72412f5ec6cea5f92a90fc9a923d
SergioPtashynski <124087817+SergioPtashynski@users.noreply.github.com>
2023-04-10 21:12:40 +0200

    start of 2 homework

---
+++ HW/index.js
@@ -0,0 +1,28 @@
+// 1
+
+//const num = prompt("Введіть число");
+//if (num % 2 === 0) {
+//    console.log ('парне')
+//} else {
+//    console.log ('не парне')
+//};
+
+// 2
+
+//const num = prompt("Введіть число");
+//if (num <= 10 && num >= 5 && num == 7 ) {
+//    console.log ('Меньше 10, більше 5 і рівно 7')
+//};
+
+// 7
+
+const number = ['20', '25', '31', '45']
+
+function getnumber (number) {
+    return `${number[number.length-1]}`
+}
+console.log (getnumber(number));
+
+
+
+

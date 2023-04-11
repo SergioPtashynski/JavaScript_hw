131b81bc07f4cef4ac8cbfa8f1a4c05c569f0d01
SergioPtashynski <124087817+SergioPtashynski@users.noreply.github.com>
2023-04-11 21:59:20 +0200

    homework day2

--- HW/index.js
+++ HW/index.js
@@ -10,19 +10,136 @@
 // 2
 
 //const num = prompt("Введіть число");
-//if (num <= 10 && num >= 5 && num == 7 ) {
-//    console.log ('Меньше 10, більше 5 і рівно 7')
+//if (num == 7){
+//    console.log ('рівно 7')
+//}
+//else if (num > 5){
+//    console.log ('більше 5')
+//}
+//else if (num < 10){
+//    console.log ('меньше 10')
 //};
+
+
+//3
+
+//const season = prompt("Впишіть місяць")
+//switch (season) {
+//    case "1":
+//        console.log("зима");
+//        break;
+//    case "2":
+//       console.log("зима");
+//        break;
+//    case "3":
+//        console.log("весна");
+//        break;
+//    case "4":
+//        console.log("весна");
+//        break;
+//    case "5":
+//        console.log("весна");
+//        break;
+//    case "6":
+//        console.log("літо");
+//        break;
+//    case "7":
+//        console.log("літо");
+//        break;
+//    case "8":
+//        console.log("літо");
+//        break;
+//    case "9":
+//        console.log("осінь");
+//        break;
+//    case "10":
+//        console.log("осінь");
+//        break;
+//    case "11":
+//        console.log("осінь");
+//        break;
+//    case "12":
+//        console.log("зима");
+//        break;
+//}
+
+
+// 4
+
+
+
+// 5
+
+
+
+// 6
+
+//const random = (min, max) =>{
+//    console.log("Мінімальне число",min, "Максимальне число", max)
+//    return Math.floor(Math.random() * (max - min) + min)
+//}
+//console.log(random(1,9999))
+
 
 // 7
 
-const number = ['20', '25', '31', '45']
+// const number = ['20', '25', '31', '45']
 
-function getnumber (number) {
-    return `${number[number.length-1]}`
-}
-console.log (getnumber(number));
+// function getnumber (number) {
+//    return `${number[number.length-1]}`
+//}
+//console.log (getnumber(number));
+
+// 8
+
+//const UserPassword = prompt("Введіть пароль");
 
 
+//if (UserPassword === 'Password' || UserPassword === 'password') {
+//    console.log("All is ok");
+//} else {
+//    console.log("All is not ok - try again");
+//}
+
+// 9
+
+//function difference(a, b) {
+//    return Math.abs(a + b);
+//  }
+  
+//  console.log(difference(3, 5));
+  
+  
+//  console.log(difference(7, 8));
+  
+  
+//  console.log(difference(8, 15));
+  
+
+// 10
+
+//function getRandomInt(min, max) {
+//    return Math.floor(Math.random() * (max - min) + min)
+//  }
+  
+//  console.log(getRandomInt(0,10));
+
+// 11
+
+//console.log(Math.floor(10.95));
 
 
+//console.log(Math.floor(2.5));
+
+
+//console.log(Math.floor(101,79));
+
+
+//console.log(Math.floor(-5.05));
+
+// 12
+
+//const number = prompt('Введіть число: ');
+
+//const result = Math.sqrt(number);
+//console.log(`Корінь числа ${number} є ${result}`);
